05cd9d781d17011575a738b4bb5f0dffa1a705f8
Aleksandra Đorđević <adjordjevic@peak-sourcing.com>
2022-02-08 07:14:24 +0100

    finish about page

--- src/app/about/aboutPage.tsx
+++ src/app/about/aboutPage.tsx
@@ -1,12 +1,10 @@
-import {Heading, Text } from '@chakra-ui/react'
+import {Heading, Text, Container } from '@chakra-ui/react'
 
-const AboutPage = () => {
-    return (
-    <>
-        <Heading>About BIT Shows</Heading>
-        <Text>This is some about description</Text>
-    </>
+const AboutPage = () =>  (
+    <Container maxWidth='70%'>
+        <Heading py={7} fontSize='2.2rem' fontWeight='400'>About BIT Shows</Heading>
+        <Text fontSize='1.5rem'>This is some about description</Text>
+    </Container>
     )
-}
 
 export default AboutPage
